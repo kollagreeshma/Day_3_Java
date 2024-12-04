fcc49eba9ac7a071e180f755b026c76efb3214bb
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-04 12:10:11 +0530

    Add files via upload

---
+++ bitevenorodd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class bitevenorodd{
+public static void main(String args[])
+{
+int n=64;
+if((n&1)==0){
+    System.out.println("It is a even number ");
+}
+else{
+    System.out.println("It is a odd number");
+}
+}
+}
