3b7a449414a94ff2e3666dd292be033056e1e433
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-04 15:23:31 +0530

    Add files via upload

---
+++ subjectmarks.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class subjectmarks{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int s1=sc.nextInt();
+int s2=sc.nextInt();
+int s3=sc.nextInt();
+int s4=sc.nextInt();
+int s5=sc.nextInt();
+String s="ram";
+boolean p=s1>=35&&s2>=35&&s3>=35&&s4>=35&&s5>=35;
+double avg=(s1+s2+s3+s4+s5)/5;
+if(avg>=80 && avg<=100&&p){
+    String f=String.format("%s your Status=PASS avg=%f and Grade=A",s,avg);
+    System.out.println(f);
+}
+else if(avg>=60 && avg<=80&&p){
+    String f=String.format("%s your Status=PASS avg=%f and Grade=B",s,avg);
+    System.out.println(f);
+}
+else if(avg>=60 && avg<=80&&p){
+    String f=String.format("%s your Status=PASS avg=%f and Grade=B",s,avg);
+    System.out.println(f);
+}
+else if(avg>=40 && avg<=60&&p){
+    String f=String.format("%s your Status=PASS avg=%f and Grade=C",s,avg);
+    System.out.println(f);
+}
+else{
+    String f=String.format("%s your Status=FAIL avg=%f and Grade=f",s,avg);
+    System.out.println(f);
+}
+}
+}
